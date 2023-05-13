740d9558d58203feb66d874016d4ed3a54e83e23
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-05-13 14:13:18 +0900

    fix: 再push

---
+++ diary-app/app/layout.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Provider from './Provider';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <head />
+      <body>
+        <Provider>
+          <Header />
+          <Main>{children}</Main>
+          <Footer />
+        </Provider>
+      </body>
+    </html>
+  );
+}
--- diary-app/prisma/migrations/20230513025912_init/migration.sql
+++ diary-app/prisma/migrations/20230513025912_init/migration.sql
@@ -14,10 +14,10 @@ CREATE TABLE `Post` (
     `title` VARCHAR(191) NOT NULL,
     `content` VARCHAR(191) NULL,
     `published` BOOLEAN NOT NULL DEFAULT false,
-    `authorId` INTEGER NULL,
+    `authorId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

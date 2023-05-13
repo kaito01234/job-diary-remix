e164e7a1d8fddc19aec9881118388c23a819320d
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-05-13 23:42:35 +0900

    add: 途中

--- diary-app/app/page.tsx
+++ diary-app/app/page.tsx
@@ -1,12 +1,44 @@
-export default function Home() {
+import { Heading } from './common/components';
+import ArticleList from './components/ArticleList';
+import type { Article } from './types';
+
+async function getArticles() {
+  // const res = await fetch('http://localhost:3000/api/articles', {
+  //   cache: 'no-store',
+  // });
+
+  // // エラーハンドリングを行うことが推奨されている
+  // if (!res.ok) {
+  //   throw new Error('Failed to fetch articles');
+  // }
+
+  // const data = await res.json();
+
+  const data = {
+    articles: [
+      {
+        id: 1,
+        title: 'string',
+        content: 'string',
+        slug: 'string',
+        createdAt: 'string',
+        updatedAt: 'string',
+      },
+    ],
+  };
+
+  return data.articles as Article[];
+}
+
+export default async function Home() {
+  const articles = await getArticles();
+
   return (
     <div>
-      <h1>新着記事</h1>
-      <ul>
-        <li>記事1</li>
-        <li>記事2</li>
-        <li>記事3</li>
-      </ul>
+      <Heading as="h1" mb={4}>
+        新着記事
+      </Heading>
+      <ArticleList articles={articles} />
     </div>
   );
 }

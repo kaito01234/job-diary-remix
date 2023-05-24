f734a1281b40260384167eea0b7893c1ac8aea5e
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-05-25 05:25:29 +0900

    add nextauth use db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import NextAuth, { SessionStrategy } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
   providers: [
@@ -7,11 +12,14 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || '',
+      clientSecret: process.env.GOOGLE_SECRET || '',
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  session: {
-    strategy: 'jwt' as SessionStrategy,
-  },
+  session: { strategy: 'jwt' as SessionStrategy },
+  adapter: PrismaAdapter(prisma),
 };
 
 const handler = NextAuth(authOptions);
